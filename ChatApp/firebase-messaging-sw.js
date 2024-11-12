dd38e0986c9de7211b576325a90f837fec680375
ethanzhall <hallethan156@gmail.com>
2024-11-11 22:03:15 -0500

    Made sure the app only sends one notif

--- ChatApp/firebase-messaging-sw.js
+++ ChatApp/firebase-messaging-sw.js
@@ -1,14 +1,2 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-    apiKey: "AIzaSyCjJzQGCZ0niMD5tek_0gLSBGJXxW0VLKA",
-    authDomain: "channelchat-7d679.firebaseapp.com",
-    projectId: "channelchat-7d679",
-    storageBucket: "channelchat-7d679.appspot.com",
-    messagingSenderId: "822894243205",
-    appId: "1:822894243205:web:8c8b1648fece9ae33e68ec"
-});
-
-const messaging = firebase.messaging();
+// Hi :)
